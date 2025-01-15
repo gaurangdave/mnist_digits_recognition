1df0bb36cb1843ae72ee3d1e9c53aaea09e26b08
Gaurang Dave <gaurangdave@Jarvis.local>
2025-01-15 10:16:53 -0800

    added placeholder webpage and hosted on fastapi app

--- api/main.py
+++ api/main.py
@@ -1,5 +1,8 @@
+from fastapi.responses import HTMLResponse
 from api.schemas.digit_data import DigitData
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from api.utils.models import load_model_and_predict
 
@@ -7,9 +10,13 @@ app = FastAPI(title="MNIST Digit Recognizer API",
               description="API for MNIST Digit Recognizer", version="0.1")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.mount("/static", StaticFiles(directory="public/static"), name="static")
+templates = Jinja2Templates(directory="public")
+
+
+@app.get("/", response_class=templates.TemplateResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post("/predict")
